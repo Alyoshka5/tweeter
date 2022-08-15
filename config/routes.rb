1e87d6eac4f854ced7b47b33d9a92d55c749744b
Alyoshka <alyoshkamyshko@gmail.com>
2022-08-15 12:18:29 -0700

    Complete Tweeter app

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
+  resources :tweets, except: [:edit, :update] do
+    resources :comments, only: [:create, :destroy]
+    member do
+      post :retweet
+    end
+  end
+  resources :profiles
+  resources :likes, only: :create
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
